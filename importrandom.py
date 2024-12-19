25a5251fffa8735260cd44d9adcd9fa459bce74a
Jeffrey Kue <jeffreyfkue@gmail.com>
2024-12-19 02:36:01 -0600

    added loop for another passwrod

--- importrandom.py
+++ importrandom.py
@@ -1,12 +1,11 @@
 import random
 
-print("randpassword")
-print("Hello! let's createa a password for you")
+print("randopassword App")
+print("Hello! let's create a password for you")
 
 
 ### Define list of random characters
 characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+" 
-numbers = "1234567890"
 
 
 ### Get the length of the password 
@@ -27,3 +26,26 @@ finalPassword = ''.join(map(str, newPassword))
 ### Print new password
 print("\n this is your new password: ", finalPassword)
     
+    
+while True:
+    user_input = input ("Do you want to generate another password? (y/n): ")
+    
+### Checks if user wants to generate another password, creates new Generation
+    if user_input in ["y", "yes"]:
+        print("\n")
+        print("Let's generate a new password!")
+        passwordLength = int(input("Enter the length of the password: "))
+        newPassword = []
+        for x in range(passwordLength):
+            newPassword.append(random.choice(characters))
+        finalPassword = ''.join(map(str, newPassword))
+        print("\n this is your new password: ", finalPassword)
+
+    elif user_input in ["n", "no"]:
+        print("Goodbye!")
+        break
+
+    else:
+        print ("\n")
+        print("Invalid input. Please type 'y' or 'n'")
+        
